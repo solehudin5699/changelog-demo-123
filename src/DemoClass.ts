3ca0ed20498a8c42ba34dcec6a873f8886452908
solehudin5699 <solehudinamqi@yahoo.co.id>
2023-05-18 15:27:21 +0700

    revert: revert x

--- src/DemoClass.ts
+++ src/DemoClass.ts
@@ -13,7 +13,10 @@ console.log('first-1');
 console.log('first-2');
 console.log('first-3');
 console.log('first-x');
+<<<<<<< HEAD
 console.log('first-y');
 console.log('first-z');
 
 const generator = () => 123;
+=======
+>>>>>>> parent of d936461 (feat: add y)
